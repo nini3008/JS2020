5f1c340d4daed885b33f05ca45a55084c92fca56
Nicholas Iloba <nick_iloba@hotmail.com>
2020-02-10 22:12:56 -0500

    moved debounce to uti;

---
+++ moviesApp/utils.js
@@ -0,0 +1,12 @@
+const debounce = (func, delay) => {
+  let timeoutID;
+  return (...args) => {
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+
+    timeoutID = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};

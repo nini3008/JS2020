f75eaae382782317403eade20744ef9ed271663d
Nicholas Iloba <nick_iloba@hotmail.com>
2020-02-11 23:44:10 -0500

    continuation of the awesome tuts

--- moviesApp/index.js
+++ moviesApp/index.js
@@ -5,13 +5,43 @@ const fetchData = async searchTerms => {
       s: searchTerms
     }
   });
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
 
-const input = document.querySelector("input");
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search For a Movie</b></label>
+<input class="input" />
+<div class="dropdown">
+<div class="dropdown-menu">
+  <div class="dropdown-content results">
+  </div>
+</div>
+</div>
+`;
 
-const onInput = event => {
-  fetchData(event.target.value);
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    option.classList.add("dropdown-item");
+    const imgSRC = movie.Poster === "N/A" ? "" : movie.Poster;
+    option.innerHTML = `
+      <img src="${imgSRC}" />
+      ${movie.Title}
+    `;
+    resultsWrapper.appendChild(option);
+  }
 };
 
 input.addEventListener("input", debounce(onInput, 500));

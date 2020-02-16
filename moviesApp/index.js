ff00c0d4b0f304c1771c2a6a27e1d7bb90c05d5a
Nicholas Iloba <nick_iloba@hotmail.com>
2020-02-16 16:48:07 -0500

    to keep me honest

--- moviesApp/index.js
+++ moviesApp/index.js
@@ -19,6 +19,12 @@ createAutoComplete({
           <img src="${imgSRC}" />
           ${movie.Title} (${movie.Year})
     `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
   }
 });
 

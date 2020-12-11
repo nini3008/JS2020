0c0a8fb981b47df810afb7c77f5336ad33587abc
Nicholas Iloba <nick_iloba@hotmail.com>
2020-12-11 16:43:08 -0500

    test

--- JSplayground/app.js
+++ JSplayground/app.js
@@ -1,13 +1,42 @@
-function testVariableScope() {
-  const name = "nick";
-  console.log(name);
-}
-
-testVariableScope();
-
-let name = " iloba  ";
-console.log(name);
 
 function reverseString(str, seperator) {
   return str.split(seperator).reverse().join(seperator);
+}
+
+
+//hackerrank JS questions
+
+//  Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
+
+function factorial(n){
+  if(n < 0){
+    return -1;
+  } else if(n == 0){
+    return 1;
+  } else{
+    return(n * factorial(n - 1))
+  }
+}
+
+// Let and Const
+
+function letConst (){
+    const PI = Math.PI
+    let r = +(readLine())
+
+    let area = PI * Math.pow(r, 2);
+    console.log(area);
+
+    let perimeter = 2 * PI * r;
+    console.log(perimeter);
+}
+
+//Arrays left rotation
+
+function rotLeft(d, arr){
+  while(d--){
+    const shift = arr.shift()
+    arr.push(shift);
+  }
+  return arr;
 }
